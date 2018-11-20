bc01fef602d7992986d82ccf5f7db2e34b787e36
kenta <hankenta01_2011@me.com>
2018-11-20 22:53:11 +0900

    評価実験用「対応付けの比較」用ベース完了

--- cgi-bin/analogy/mypackage/response.py
+++ cgi-bin/analogy/mypackage/response.py
@@ -6,23 +6,23 @@ def Randomname(n):
    return ''.join(randlst)
 
 ## 承認コード，乱数生成
-def Random_Res():
-    random_json = {"randomname":""}
-    rand = Randomname(12)
-    random_json["randomname"] = rand
-    return random_json,rand
+def Response_Random():
+    json_random = {"randomname":""}
+    random = Randomname(12)
+    json_random["randomname"] = random
+    return random,json_random
 
-## 絶対的な特徴のjson形式整理
-def Category_Res(cate):
-    cate_json = []
+## 絶対的な特徴（カテゴリ）のjson形式整理
+def Response_Category(cate):
+    json_category = []
     temp_sql_word = []
     sql_unvis,sql_vis,sql_word = [],[],""
     for i in range(len(cate)):
-        response_catejson = {"cate_unspot":"","cate_vispot":"","cate_word":""}
-        response_catejson["cate_unspot"] = cate[i][0]
+        response_catejson = {"unvis_name_c":"","vis_name_c":"","word_c":""}
+        response_catejson["unvis_name_c"] = cate[i][0]
         sql_unvis.append(cate[i][0])
 
-        response_catejson["cate_vispot"] = cate[i][1]
+        response_catejson["vis_name_c"] = cate[i][1]
         sql_vis.append(cate[i][1])
 
         temp = []
@@ -33,7 +33,7 @@ def Category_Res(cate):
                 temp.append(cate[i][2][j][0])
             except TypeError:
                 continue
-        response_catejson["cate_word"] = word_list
+        response_catejson["word_c"] = word_list
         temp_sql_word.append(temp)
 
         for l in range(len(temp_sql_word)):
@@ -41,35 +41,32 @@ def Category_Res(cate):
             sql_word += tmp
         sql_word = sql_word[:-4]
 
-        cate_json.append(response_catejson)
-    return cate_json,sql_unvis,sql_vis,sql_word
+        json_category.append(response_catejson)
+    return sql_unvis,sql_vis,sql_word,json_category
 
-##　メイン，相対的な特徴のjson形式整理
-def Response(data,name,lat,lng,cate):
-    random_json,rand = Random_Res()
-    cate_json,sql_cate_unvis,sql_cate_vis,sql_cate_word = Category_Res(cate)
-
-    all_json = []
+##　絶対的な特徴（特徴ベクトル）のjson形式整理
+def Response_Vector_Feature(data,name,lat,lng):
+    json_feature = []
     temp_sql_word = []
-    sql_unvis,sql_vis,sql_cossim,sql_lat,sql_lng,sql_word,sql_code = [],[],[],[],[],"",rand
+    sql_unvis,sql_vis,sql_cossim,sql_lat,sql_lng,sql_word = [],[],[],[],[],""
 
     for i in range(len(data)):
-        response_json = {"unvis_name":"","vis_name":"","cossim":"","unvis_lat":"","unvis_lng":"","word":""}
+        response_json = {"unvis_name_f":"","vis_name_f":"","cossim_f":"","unvis_lat_f":"","unvis_lng_f":"","word_f":""}
 
-        response_json["unvis_name"] = data[i][0]
+        response_json["unvis_name_f"] = data[i][0]
         sql_unvis.append(data[i][0])
 
-        response_json["vis_name"] = data[i][1]
+        response_json["vis_name_f"] = data[i][1]
         sql_vis.append(data[i][1])
 
-        response_json["cossim"] = data[i][2]
+        response_json["cossim_f"] = data[i][2]
         sql_cossim.append(str(data[i][2]))
 
         for k in range(len(name)):
             if data[i][0] == name[k]:
-                response_json["unvis_lat"] = lat[k]
+                response_json["unvis_lat_f"] = lat[k]
                 sql_lat.append(lat[k])
-                response_json["unvis_lng"] = lng[k]
+                response_json["unvis_lng_f"] = lng[k]
                 sql_lng.append(lng[k])
 
         word_list = []
@@ -80,17 +77,67 @@ def Response(data,name,lat,lng,cate):
                 temp.append(data[i][3][j][0])
             except TypeError:
                 continue
-        response_json["word"] = word_list
+        response_json["word_f"] = word_list
         temp_sql_word.append(temp)
 
-        all_json.append(response_json)
-
-    all_json = [all_json] + [cate_json] + [random_json]
-    print(json.dumps(all_json)) ## 送信
+        json_feature.append(response_json)
 
     for l in range(len(temp_sql_word)):
         tmp = "，".join(temp_sql_word[l]) + " -- "
         sql_word += tmp
     sql_word = sql_word[:-4]
 
-    return sql_unvis,sql_vis,sql_cossim,sql_lat,sql_lng,sql_word,sql_code,sql_cate_unvis,sql_cate_vis,sql_cate_word
+    return sql_unvis,sql_vis,sql_cossim,sql_lat,sql_lng,sql_word,json_feature
+
+
+##　相対的な特徴（差分ベクトル）のjson形式整理
+def Response_Vector(data,name,lat,lng):
+    json_vector = []
+    temp_sql_word = []
+    sql_unvis,sql_vis,sql_cossim,sql_lat,sql_lng,sql_word = [],[],[],[],[],""
+
+    for i in range(len(data)):
+        response_json = {"unvis_name_v":"","vis_name_v":"","cossim_v":"","unvis_lat_v":"","unvis_lng_v":"","word_v":""}
+
+        response_json["unvis_name_v"] = data[i][0]
+        sql_unvis.append(data[i][0])
+
+        response_json["vis_name_v"] = data[i][1]
+        sql_vis.append(data[i][1])
+
+        response_json["cossim_v"] = data[i][2]
+        sql_cossim.append(str(data[i][2]))
+
+        for k in range(len(name)):
+            if data[i][0] == name[k]:
+                response_json["unvis_lat_v"] = lat[k]
+                sql_lat.append(lat[k])
+                response_json["unvis_lng_v"] = lng[k]
+                sql_lng.append(lng[k])
+
+        word_list = []
+        temp = []
+        for j in range(len(data[i][3])):
+            try:
+                word_list.append(data[i][3][j][0])
+                temp.append(data[i][3][j][0])
+            except TypeError:
+                continue
+        response_json["word_v"] = word_list
+        temp_sql_word.append(temp)
+
+        json_vector.append(response_json)
+
+    for l in range(len(temp_sql_word)):
+        tmp = "，".join(temp_sql_word[l]) + " -- "
+        sql_word += tmp
+    sql_word = sql_word[:-4]
+
+    return sql_unvis,sql_vis,sql_cossim,sql_lat,sql_lng,sql_word,json_vector
+
+def Response(category,feature,vector,random,record_id):
+    json_record = {"record_id":""}
+    json_record["record_id"] = record_id
+    all_json = []
+    all_json = [category] + [feature] + [vector] + [random] + [json_record]
+    print(json.dumps(all_json)) ## 送信
